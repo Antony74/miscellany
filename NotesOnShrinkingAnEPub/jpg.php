49a3104b8f2a5c0f604c34206311ee96ce66171e
Antony74 <akb@akb.me.uk>
2016-02-27 17:57:54 +0000

    Add some notes on converting it to .mobi

--- NotesOnShrinkingAnEPub/jpg.php
+++ NotesOnShrinkingAnEPub/jpg.php
@@ -1,8 +1,8 @@
 <?php
 
 // How to shrink an epub (Windows)
-// 0. All software mentioned can be installed with Chocolately
-// 1. Sigil has two "Delete unused" options on its "Tools" menu
+// 0. All software mentioned can be installed with Chocolately (kindlestrip can be installed with pip which can be installed with Chocolately)
+// 1. (SKIP: REQUIRES GUI) Sigil has two "Delete unused" options on its "Tools" menu
 // 2. Unzip with 7-Zip
 // 3. 
 
@@ -13,6 +13,10 @@ foreach(glob('D:\git\vegan\OEBPS\Images\*.jpg') as $sFilename)
 
 // 4. pngquant --ext .png --force --quality 40-50 *.png
 // 5. Zip with 7-zip
-// 6. Leanify
+// 6. (SKIP: MARGINAL) Leanify
+
+// How to convert to .mobi (e.g. for a Kindle device), then strip out the original file
+// 7. KindleGen
+// 8. kindlestrip input.mobi output.mobi
 
 ?>
