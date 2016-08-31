cce6574a99008aa69efdb1f771d2792108d7f1a3
Antony <akb@akb.me.uk>
2016-08-31 14:07:23 +0100

    Result is starting to look almost sensible.  I wonder if I'd have more luck with Beziers.

--- CurveFitting/optim.r
+++ CurveFitting/optim.r
@@ -15,20 +15,25 @@ addPoint( 117 , 169 );
 addPoint( 86 , 214 );
 addPoint( 40 , 200 );names(points) = c("x", "y");
 
-bFirstTime = TRUE;
-
 f = function(params)
 {
 	a = params[1];
 	b = params[2];
-	c = params[3];
+	cc = params[3];
 	d = params[4];
-	e = params[4];
+	e = params[5];
 	f = params[6];
 	g = params[7];
 	h = params[8];
 
 	tValues = params[9:length(params)];
+
+	cubic = function(t)
+	{
+		x = (a * t * t * t) + (b * t * t) + (cc * t) + d;
+		y = (e * t * t * t) + (f * t * t) + (g  * t) + h;
+		return(c(x, y));
+	};
 
 	comparision = cbind(points, t = tValues);
 
@@ -40,26 +45,39 @@ f = function(params)
 		targetY = comparision$y[nRow];
 		t = comparision$t[nRow];
 
-		x = (a * t * t * t) + (b * t * t) + (c * t) + d;
-		y = (e * t * t * t) + (f * t * t) + (g * t) + h;
+		xy = cubic(t);
 
-		x = x - targetX;
-		y = y - targetY;
+		x = xy[1] - targetX;
+		y = xy[2] - targetY;
 
 		distance = (x * x) + (y * y);
 
 		distances <<- distances + distance;
 	});
 
-	if (bFirstTime)
-	{
-		print(distances);
-		bFirstTime <<- FALSE;
-	}
+	start = cubic(0);
+	end   = cubic(1);
 
-	return(distances);
+	x = end[1] - start[1];
+	y = end[2] - start[2];
+
+	distance = (x * x) + (y * y);
+
+	return(distances + (sqrt(distance)^1.5));
 }
 
 params = rep(0.5, 8 + nrow(points));
-result = nlm(f, params);
+result = optim(params, f,
+			   method = "L-BFGS-B",
+			   lower = c(rep(-Inf,8), rep(0, nrow(points))),
+			   upper = c(rep(+Inf,8), rep(1, nrow(points))));
+
+cat(paste("a = ", result$par[1]), ';\n', sep="");
+cat(paste("b = ", result$par[2]), ';\n', sep="");
+cat(paste("c = ", result$par[3]), ';\n', sep="");
+cat(paste("d = ", result$par[4]), ';\n', sep="");
+cat(paste("e = ", result$par[5]), ';\n', sep="");
+cat(paste("f = ", result$par[6]), ';\n', sep="");
+cat(paste("g = ", result$par[7]), ';\n', sep="");
+cat(paste("h = ", result$par[8]), ';\n', sep="");
 
